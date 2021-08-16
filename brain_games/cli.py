e6d66ea44d362872cd91b1a1b6206dcc9caf0268
ya.grigorev <ya.grigorev@tendertech.ru>
2021-08-16 23:57:06 +0300

    Исправление на отдыкватный flake8

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,15 +1,11 @@
 import prompt
 
 
-# noinspection TaskProblemsInspection,SpellCheckingInspection
 def welcome_user():
-    """ddfdf."""
     name = prompt.string('May I have your name? ')
     template = 'Hello, {}!'
-    # noinspection TaskProblemsInspection
     print(template.format(name))
 
 
 if __name__ == '__main__':
-
     welcome_user()
