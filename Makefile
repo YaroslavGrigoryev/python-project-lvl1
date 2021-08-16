864d79cb2d08dd5a51da633dcb82f9a86e377e2f
ya.grigorev <ya.grigorev@tendertech.ru>
2021-08-16 23:49:37 +0300

    Проверка flake8

--- Makefile
+++ Makefile
@@ -13,4 +13,5 @@ publich:
 package-install:
 	python3 -m pip install --user dist/*.whl --force-reinstall
 
-
+lint:
+	poetry run flake8 brain_games
--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,9 +1,15 @@
 import prompt
 
+
+# noinspection TaskProblemsInspection,SpellCheckingInspection
 def welcome_user():
-	print("")
-	name = prompt.string('May I have your name? ')
-	print("Hello, " + name + "!")
+    """ddfdf."""
+    name = prompt.string('May I have your name? ')
+    template = 'Hello, {}!'
+    # noinspection TaskProblemsInspection
+    print(template.format(name))
+
 
 if __name__ == '__main__':
-	welcome_user()
+
+    welcome_user()
